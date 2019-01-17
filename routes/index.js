725164bd9f142fecb7af9bca543476b1a81b6fb8
Daniel Torres <torresestradadaniel@gmail.com>
2019-01-16 23:17:46 -0600

    First commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var ocb = require("ocb-sender");
+var ngsi = require("ngsi-parser");
+
+ocb.config('http://0.0.0.0:1026/v2')
+.then((result) => console.log(result))
+.catch((err) => console.log(err));
+
+router.get('/entities', function(req, res, next) {
+  console.log(req.query)
+  let query = ngsi.createQuery({
+    "id":"Room.*",
+    "type":"Room",
+    "options":"keyValues",
+   "temperature" : req.query.temperature
+  })
+  ocb.getWithQuery(query)
+  .then((result) => {
+    res.render('index', { entities: result.body });
+  })
+  .catch((err) => {
+    res.render('index', { entities: {} });
+  })
+});
+
+router.post('/notify', function(req, res, next) {
+  console.log(req.body.data[0]);
+  res.status(200).send("OK");
+});
+
+module.exports = router;
